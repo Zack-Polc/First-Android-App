4ba4cd18fbb86803f01d87d7066f281db21b6fed
Zack Polc <zack.polc@gmail.com>
2018-08-20 12:15:01 +0100

    added gif animation - to be changed in the future

--- app/src/main/java/com/example/zpolc/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/zpolc/myfirstapp/MainActivity.kt
@@ -3,7 +3,8 @@ package com.example.zpolc.myfirstapp
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
+import android.widget.ImageView
+import com.koushikdutta.ion.Ion
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,22 +12,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val gif = findViewById<ImageView>(R.id.imageView)
+        Ion.with(gif).load("https://78.media.tumblr.com/tumblr_m0v7zqEX2l1r03eaxo1_r1_500.gif")
     }
 
-    fun toast(view: View) {
-                                        // (this, message, duration)
-        val myToast = Toast.makeText(this, "sup", Toast.LENGTH_SHORT)
-        myToast.show()
-    }
+//    fun toast(view: View) {
+//                                        // (this, message, duration)
+//        val myToast = Toast.makeText(this, "sup", Toast.LENGTH_SHORT)
+//        myToast.show()
+//    }
 
     fun counter(view: View) {
         val showCounterTextView = counter
 
         val countString = showCounterTextView.text.toString()
 
-        // converting string into int and then display it <- this is a fking great comment btw
+        // converting string into int and then display it
         var count: Int = Integer.parseInt(countString)
         count++
+        showCounterTextView.text = count.toString()
+    }
+
+    fun counterReset(view: View) {
+        val showCounterTextView = counter
+
+        val countString = showCounterTextView.text.toString()
+
+        // converting string into int and then display it
+        var count: Int = Integer.parseInt(countString)
+        count = 0
         showCounterTextView.text = count.toString()
     }
 }
